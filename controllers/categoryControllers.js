cdab11d8cc6f4cc0cb1cce01d01bbbedda7df601
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-12-10 22:15:05 +0700

    Finish a category editing page

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -26,7 +26,6 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 		.sort({ name: 1 })
 		.exec();
 
-	console.log(result);
 	res.render("category_detail", { products: result, category: category });
 });
 
@@ -36,16 +35,24 @@ exports.category_add_get = asyncHandler(async (req, res, next) => {
 exports.category_add_post = [
 	body("name", "Invalid character or the first letter is not capitalized.")
 		.trim()
+		.isLength({ min: 1, max: 30 })
 		.isAlpha("en-US", { ignore: " " })
 		.matches(/^[A-Z].*$/gm)
+		.custom(async (v) => {
+			const result = await Category.findOne({ name: v }).exec();
+
+			if (result !== null) throw new Error("Category already exists!");
+		})
 		.escape(),
 	asyncHandler(async (req, res, next) => {
 		const result = validationResult(req);
 
 		if (!result.isEmpty()) {
 			// If error it goes here
-			res.sendStatus(404);
-			res.render("category_add", { body: req.body });
+			res.render("category_add", {
+				category_name: req.body.name,
+				errorMsg: result.errors,
+			});
 			return;
 		}
 
@@ -60,11 +67,61 @@ exports.category_add_post = [
 	}),
 ];
 exports.category_edit_get = asyncHandler(async (req, res, next) => {
-	res.sendStatus(202);
+	const receivedName = req.params.name;
+	const category = await Category.findOne(
+		{ name: receivedName },
+		{ _id: 1, name: 1 }
+	).exec();
+
+	if (!category) {
+		// Will go here and end the function if not found
+		res.sendStatus(404);
+		return;
+	}
+
+	res.render("category_edit", { category_name: category.name });
 });
-exports.category_edit_post = asyncHandler(async (req, res, next) => {
-	res.write("To be implemented");
-});
+exports.category_edit_post = [
+	body("name", "Invalid character or the first letter is not capitalized.")
+		.trim()
+		.isLength({ min: 1, max: 30 })
+		.isAlpha("en-US", { ignore: " " })
+		.matches(/^[A-Z].*$/gm)
+		.custom(async (v, { req }) => {
+			if (v === req.params.name) {
+				// In case users didn't change the name
+				throw new Error("You must change the name!");
+			}
+
+			const result = await Category.findOne({ name: v }).exec();
+			if (result !== null) throw new Error("Category already exists!");
+		})
+		.escape(),
+	asyncHandler(async (req, res, next) => {
+		const result = validationResult(req);
+
+		if (!result.isEmpty()) {
+			// If error it goes here
+			res.render("category_edit", {
+				category_name: req.body.name,
+				errorMsg: result.errors,
+			});
+			return;
+		}
+
+		const category_name = req.params.name;
+		const nameToChange = req.body.name;
+		const updateResult = await Category.findOneAndUpdate(
+			{ name: category_name },
+			{ name: nameToChange },
+			{
+				new: true,
+			}
+		);
+
+		res.redirect(updateResult.url);
+	}),
+];
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
 	// To be implemented
 });
